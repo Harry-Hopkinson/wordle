3d751c163666e1df7fbb258403dc3bcc1e5cc986
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-01-26 11:31:45 +0000

    Logic for checking the length of a word

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Setup Project!");
+const prompts = require("prompts");
+const chalk = require("chalk");
+const wordList = require("./words.json")
+
+var puzzle = "";
+
+const wordlePrompt = {
+    type: "text",
+    name: "word",
+    message: "Enter a 5 Letter Word...",
+    validate: value => value.length != 5 ? "Word must be 5 Letters" : true
+};
