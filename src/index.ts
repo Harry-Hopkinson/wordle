39ae93cb3f19f25829de7f29b911060cb529e066
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-01-26 11:34:39 +0000

    Check if word is valid

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,23 @@ const wordlePrompt = {
     message: "Enter a 5 Letter Word...",
     validate: value => value.length != 5 ? "Word must be 5 Letters" : true
 };
+
+async function check(guess) {
+    var results = [];
+
+    for (let i in guess) {
+        var attempt = { letter: guess[i], color: "bgGrey" };
+        
+        if (attempt.letter === puzzle[i]) {
+            process.stdout.write(chalk.white.bgGreen.bold(` ${guess[i]} \t`));
+            continue; 
+        }
+
+        if (puzzle.includes(attempt.letter)) {
+            process.stdout.write(chalk.white.bgYellow.bold(` ${guess[i]} \t`));
+            continue;
+        }
+        process.stdout.write(chalk.white.bgGrey.bold(` ${guess[i]} \t`));
+    }
+    return results;
+}
